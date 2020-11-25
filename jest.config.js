47cebecc14baf2ff36761df243d04201722f0337
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-11-24 22:40:46 -0300

    chore: jest config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  root: ['<rootDir>/src'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
