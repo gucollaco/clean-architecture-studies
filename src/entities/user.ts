8c0731ac0a22620164dc33dc39582d032d7db0f7
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-22 00:36:12 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,16 +1,29 @@
 import { UserData } from './user-data'
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { Email } from './email'
 import { Name } from './name'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.email = email
+    this.name = name
+  }
+
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) return left(new InvalidNameError())
 
     const emailOrError = Email.create(userData.email)
     if (emailOrError.isLeft()) return left(new InvalidEmailError())
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
