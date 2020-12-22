f100ac9fcb2bc6b9dcb4eb35948fd9bad39cd4dd
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-21 23:49:19 -0300

    feat: verify name while creating user

---
+++ src/entities/name.ts
@@ -0,0 +1,23 @@
+import { left, right } from '../shared/either'
+import { InvalidNameError } from './errors/invalid-name-error'
+
+export class Name {
+  private readonly name: string
+
+  private constructor (name: string) {
+    this.name = name
+  }
+
+  static create (name: string) {
+    if (!Name.validate(name)) return left(new InvalidNameError())
+
+    return right(new Name(name))
+  }
+
+  static validate (name: string): boolean {
+    if (!name) return false
+    if (name.trim().length < 2 || name.trim().length > 256) return false
+
+    return true
+  }
+}
