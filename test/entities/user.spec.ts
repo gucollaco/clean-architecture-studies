eacf0dfcf2c62aa3ee1258483b1a6c61f9b54c5a
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-01-26 22:11:31 -0300

    refactor: simplify imports

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -1,4 +1,4 @@
-import { User } from '../../src/entities/user'
+import { User } from '../../src/entities'
 
 describe('User domain entities', () => {
   test('Should not create user with invalid e-mail address', () => {
