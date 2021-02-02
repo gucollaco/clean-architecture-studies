61a7e81114d24d8ebd24d8308be65915c6fb7a1f
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-02-02 00:10:10 -0300

    feat: web controller should return 500 when server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,30 +1,34 @@
 import { UserData } from '@/entities'
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { MissingParamError } from './errors/missing-param-error'
-import { created, badRequest } from './utils/http-helper'
+import { MissingParamError } from '@/web-controllers/errors/missing-param-error'
+import { created, badRequest, serverError } from '@/web-controllers/utils/http-helper'
+import { UseCase } from '@/usecases/ports'
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList
+  private readonly usecase: UseCase
 
-  constructor (usecase: RegisterUserOnMailingList) {
+  constructor (usecase: UseCase) {
     this.usecase = usecase
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
-    const name = request.body.name
-    const email = request.body.email
-    if (!name || !email) {
-      const missingNameParam = !name ? 'name ' : ''
-      const missingEmailParam = !email ? 'email' : ''
-      const missingParam = missingNameParam + missingEmailParam
-      return badRequest(new MissingParamError(missingParam.trim()))
+    try {
+      const name = request.body.name
+      const email = request.body.email
+      if (!name || !email) {
+        const missingNameParam = !name ? 'name ' : ''
+        const missingEmailParam = !email ? 'email' : ''
+        const missingParam = missingNameParam + missingEmailParam
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
+
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
+
+      if (response.isLeft()) return badRequest(response.value)
+      if (response.isRight()) return created(response.value)
+    } catch (error) {
+      return serverError(error)
     }
-
-    const userData: UserData = request.body
-    const response = await this.usecase.registerUserOnMailingList(userData)
-
-    if (response.isLeft()) return badRequest(response.value)
-    if (response.isRight()) return created(response.value)
   }
 }
