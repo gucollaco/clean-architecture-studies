5301fbfbea0a47a9295344bd03194d029864bd67
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-02-09 23:06:26 -0300

    test: config register route

---
+++ test/main/config/routes/register-route.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Register route', () => {
+  test('Should return an account on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app).post('/api/register').send({ name: 'Any name', email: 'any@mail.com' }).expect(201)
+  })
+})
