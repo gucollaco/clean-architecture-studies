8d50c0c6e3ecc8337643582445ef78324f99c0b1
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-02-17 01:24:18 -0300

    fix: log message

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
 import 'module-alias/register'
 import app from '@/main/config/app'
 
-app.listen(3000, () => console.log('Server running at http://localhost/5000'))
+app.listen(3000, () => console.log('Server running at http://localhost:3000'))
