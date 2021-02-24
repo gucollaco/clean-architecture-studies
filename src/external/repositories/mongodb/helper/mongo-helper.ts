3a2e1d5463117de006b76861042ac9a4b9c21d71
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-02-23 21:01:25 -0300

    fix: helper export name

--- src/external/repositories/mongodb/helper/mongo-helper.ts
+++ src/external/repositories/mongodb/helper/mongo-helper.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Collection } from 'mongodb'
 
-export const mongoHelper = {
+export const MongoHelper = {
   client: null as MongoClient,
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri, {
